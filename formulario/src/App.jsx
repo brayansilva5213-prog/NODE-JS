c823281f55b5a6f3e2c81d492ad36b09f737bfeb
Brayan <brayansilva5213@gmail.com>
2026-05-14 10:58:23 -0300

    feat:kickoff

---
+++ formulario/src/App.jsx
@@ -0,0 +1,18 @@
+function App()  {
+  return  (
+     <div>
+        <h1>Ola, mundo!</h1>
+     </div>
+  )
+} 
+export default App
+
+function cabecalho() {
+  return (
+    <header>
+      <h1>Meu sistema</h1>
+      <p>Bem-vindo</p>
+     </header>
+  )
+}
+export default App
---
+++ formulario/src/cabecalho.jsx
@@ -0,0 +1,10 @@
+import App from "../App";
+function cabecalho() {
+  return (
+    <header>
+      <h1>Meu sistema</h1>
+      <p>Bem-vindo</p>
+     </header>
+  )
+}
+export default App
